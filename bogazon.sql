214cd9a7004302a68fc2b19961a0e6deeb6352fc
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-06-21 11:48:15 -0500

    correct primary key

--- bogazon.sql
+++ bogazon.sql
@@ -9,11 +9,12 @@ USE bogazon_DB;
 
 -- Create Table
 CREATE TABLE products(
-    product_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(30) NOT NULL,
     category_name VARCHAR(30) NOT NULL,
     price DECIMAL(10,2) NULL,
     quantity INT NULL,
+    PRIMARY KEY (product_id)
 );
 
 -- Add Initial Products
