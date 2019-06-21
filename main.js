50030c1f87bbab69c10fa500b9a76adbfce92b6a
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-06-21 12:17:46 -0500

    add mysql connection

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 // Dependencies
 var mysql = require("mysql");
+var inquirer = require("inquiere"):
+
+// Create connection to SQL Database
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "********",
+    database: "bogazon_DB"
+});
