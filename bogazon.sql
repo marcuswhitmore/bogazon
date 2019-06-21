ccae0bdf5bac13e60e21587a2b36152d0f671cf1
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-06-21 11:22:51 -0500

    sql create table

--- bogazon.sql
+++ bogazon.sql
@@ -7,5 +7,12 @@ CREATE DATABASE bogazon_DB;
 
 USE bamazon-DB;
 
-
+-- Create Table
+CREATE TABLE products(
+    product_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(30) NOT NULL,
+    category_name VARCHAR(30) NOT NULL,
+    price DECIMAL(10,2) NULL,
+    quantity INT NULL,
+);
 
